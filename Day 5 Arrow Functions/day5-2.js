d72f9067eaa690b69ba85a89826104ebe13afeef
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-09-08 23:59:21 +0530

    day5-2.js

--- Day 5 Arrow Functions/day5-2.js
+++ Day 5 Arrow Functions/day5-2.js
@@ -1,2 +1,28 @@
 // code snippet Day 5: Arrow Functions
 
+
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+    
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] % 2 == 0){
+            nums[i] *= 2;
+        }else{
+            nums[i] *= 3;
+        }
+    }
+    return nums;
+}
+
+
+function main() {
+    const n = +(readLine());
+    const a = readLine().split(' ').map(Number);
+    
+    console.log(modifyArray(a).toString().split(',').join(' '));
+}

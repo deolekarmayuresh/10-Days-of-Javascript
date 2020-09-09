77315f5e4c192979e184cb19485956ccd3e5e563
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-09-09 20:45:31 +0530

    day 6-1.js

--- Day 6 JavaScript Dates/day 6-1.js
+++ Day 6 JavaScript Dates/day 6-1.js
@@ -1,2 +1,23 @@
 // code snippet Day 6: JavaScript Dates
 
+
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    // array of days
+    let dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let date = new Date(dateString);  
+    
+    return dayName[date.getDay()];
+}
+
+
+function main() {
+    const d = +(readLine());
+    
+    for (let i = 0; i < d; i++) {
+        const date = readLine();
+        
+        console.log(getDayName(date));
+    }
+}

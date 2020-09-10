28fdf3be87c151954850da57e61957ad616a1109
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-09-11 00:05:52 +0530

    day 7 regular expressions-iii

--- Day 7 Regular Expressions III/day 7-2.js
+++ Day 7 Regular Expressions III/day 7-2.js
@@ -1,2 +1,26 @@
 // code snippet Day 7: Regular Expressions III
 
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+    
+    
+    /*
+     * Do not remove the return statement
+     */
+    return RegExp('\\d+','g');
+}
+
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    
+    const r = s.match(re);
+    
+    for (const e of r) {
+        console.log(e);
+    }
+}

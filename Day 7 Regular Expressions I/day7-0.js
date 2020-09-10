e84f0a9116b0ffaa3dbe4056638f0ea3946be2bb
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-09-11 00:02:03 +0530

    day 7-0.js

--- Day 7 Regular Expressions I/day7-0.js
+++ Day 7 Regular Expressions I/day7-0.js
@@ -1,2 +1,22 @@
 // code snippet Day 7: Regular Expressions I
 
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+     */
+    
+    return /^([aeiou]).*\1$/;
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    
+    console.log(re.test(s));
+}

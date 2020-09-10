3b7d5694e0b4ab010abd9cf60986c13514d4e19c
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-09-11 00:03:55 +0530

    day 7 regular experssions-ii

--- Day 7 Regular Expressions II/day7-1.js
+++ Day 7 Regular Expressions II/day7-1.js
@@ -1,2 +1,23 @@
 // code snippet Day 7: Regular Expressions II
 
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
+     * followed by one or more letters.
+     */
+    
+    return /^(Mr|Mrs|Ms|Dr|Er)\.[a-zA-Z]*$/;
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    
+    console.log(!!s.match(re));
+}
